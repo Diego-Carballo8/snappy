994474cd285ae25fc854ff538efc182aabdbe1a7
Diego-Carballo8 <diego.carballo.alp@cbtis258.edu.mx>
2025-06-05 17:37:29 -0600

    snappy.js

---
+++ snappy.js
@@ -0,0 +1,15 @@
+/*button envia al perfil del usuario*/
+document.getElementById('perfilButton').addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = 'perfil.html';  // te dirige a inicio.html
+});
+/*button envia al perfil del usuario*/
+document.getElementById('snappyButton').addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = 'snappy.html';  // te dirige a inicio.html
+});
+/*button envia al perfil del usuario*/
+document.getElementById('buscarButton').addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = 'buscar.html';  // te dirige a inicio.html
+});
